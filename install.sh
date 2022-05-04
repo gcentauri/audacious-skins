5f1aba542d1da3647153e5aa9e1935db649e782c
The Infinite Rick <online.accts.666@gmail.com>
2022-05-04 09:53:56 -0400

    First Commit

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if [ "$(id -u)" -ne 0 ]; then
+	echo "must run script with root privileges"
+	exit 1
+fi
+
+if ! command -v audacious >/dev/null; then
+    echo "Audacious is not installed"
+    exit 1
+fi
+
+SRC_DIR="./skins"
+
+if [ ! -d "${SRC_DIR}" ]; then
+    echo "skins source directory does not exist"
+    exit 1
+fi
+
+DST_DIR="/usr/share/audacious/Skins"
+
+if [ -d "${DST_DIR}" ]; then
+    rm -f "${DST_DIR}"
+fi
+
+mkdir -p "${DST_DIR}"
+
+cp -f "${SRC_DIR}"/* "${DST_DIR}"
+
+echo "Audacious skins have been successfully installed"
